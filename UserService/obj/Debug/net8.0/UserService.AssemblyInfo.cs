781e85590f9c4f5ea45f8d3f7f82deba4e1d0fe6
M'bazie Kone <mbazie89@gmail.com>
2025-01-31 00:22:43 +0100

    Updates

--- UserService/obj/Debug/net8.0/UserService.AssemblyInfo.cs
+++ UserService/obj/Debug/net8.0/UserService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f6977dbd752c0f087356f6f36eb3b3842726738")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2206c5f4fd4b69b9484ef0f151e09d7c9886357")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
