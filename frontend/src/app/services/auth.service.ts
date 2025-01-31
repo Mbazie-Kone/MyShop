ea09504207e58341d45c0a24bcbf65200b83039d
M'bazie Kone <mbazie89@gmail.com>
2025-01-31 22:54:48 +0100

    Updates

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl = '/api/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string): Observable<any> {
+
+    return this.http.post(`${this.apiUrl}/login`, { email, password });
+  }
 }
