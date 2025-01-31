c452df9bc4522d10e969cd1483369bbcd16dd67b
M'bazie Kone <mbazie89@gmail.com>
2025-01-31 23:18:19 +0100

    Updates

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email = '';
+  password = '';
 
+  constructor(private authService: AuthService) {}
+
+  login() {
+    this.authService.login(this.email, this.password).subscribe(response => {
+      localStorage.setItem('token', response.token);
+      alert('login effettuato con successo!');
+    })
+  }
 }
