67751d01dac09dc34eddea9ac3d3e83d874b5a33
M'bazie Kone <mbazie89@gmail.com>
2025-02-01 02:03:46 +0100

    Updates

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './components/login/login.component';
 import { ErrorComponent } from './components/error/error.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: '', component: LoginComponent},
   { path: 'login', component: LoginComponent},
   { path: '**', component: ErrorComponent}
 ];
