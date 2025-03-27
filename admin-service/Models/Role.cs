f4463b9c1b22de81052b822eb0a3d21858459c68
M'bazie Kone <mbazie89@gmail.com>
2025-03-28 00:59:03 +0100

    Updates

--- admin-service/Models/Role.cs
+++ admin-service/Models/Role.cs
@@ -2,5 +2,11 @@
 {
     public class Role
     {
+        public long Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public ICollection<User>? Users { get; set; }
     }
 }
