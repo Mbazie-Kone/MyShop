132ae17235fc6f1976defae17d4f0ababc874fa8
M'bazie Kone <mbazie89@gmail.com>
2025-03-28 03:16:43 +0100

    Updates

---
+++ admin-service/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using admin_service.Data;
+using admin_service.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace admin_service.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AdminController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AdminController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser(User user)
+        {
+            user.CreatedAt = DateTime.Now;
+            user.UpdatedAt = DateTime.Now;
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+    }
+}
