ee90ab4de0ce67a1dc4f56b0539964534f31f3b7
M'bazie Kone <mbazie89@gmail.com>
2025-03-28 03:28:54 +0100

    Updates

---
+++ admin-service/DTOs/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace admin_service.DTOs
+{
+    public class UserDto
+    {
+        public long Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        
+        public DateTime CreatedAt { get; set}
+    }
+}
