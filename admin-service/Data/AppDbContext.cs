e910b7620d5d3ed33a7dc9bccb10799000da07f8
M'bazie Kone <mbazie89@gmail.com>
2025-03-28 02:17:51 +0100

    Updates

--- admin-service/Data/AppDbContext.cs
+++ admin-service/Data/AppDbContext.cs
@@ -1,6 +1,18 @@
-﻿namespace admin_service.Data
+﻿using admin_service.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace admin_service.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Role> Roles => Set<Role>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
     }
 }
