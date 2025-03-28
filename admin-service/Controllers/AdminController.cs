7e58d2d2e34d20164365f1ff0daf4a5c6e6117bf
M'bazie Kone <mbazie89@gmail.com>
2025-03-28 03:18:32 +0100

    Updates

--- admin-service/Controllers/AdminController.cs
+++ admin-service/Controllers/AdminController.cs
@@ -15,6 +15,7 @@ namespace admin_service.Controllers
             _context = context;
         }
 
+        // POST: api/admin
         [HttpPost]
         public async Task<ActionResult<User>> CreateUser(User user)
         {
