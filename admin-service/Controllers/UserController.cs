70f1f3a01a6c63890fa0cda9bc37904cb63b248d
M'bazie Kone <mbazie89@gmail.com>
2025-03-28 02:56:53 +0100

    Updates

---
+++ admin-service/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using admin_service.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace admin_service.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
