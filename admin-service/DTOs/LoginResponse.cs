0184a4aee18ea4d307d502773d6163003729f730
M'bazie Kone <mbazie89@gmail.com>
2025-03-29 08:20:58 +0100

    Updates

---
+++ admin-service/DTOs/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace admin_service.DTOs
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
