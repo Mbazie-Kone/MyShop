81e3542c3b6dcc2de8ed3747d0e0e1d9270c5198
M'bazie Kone <mbazie89@gmail.com>
2025-03-29 22:35:27 +0100

    Updates

---
+++ backend/admin-service/Models/Role.cs
@@ -0,0 +1,13 @@
+﻿namespace admin_service.Models
+{
+    public class Role
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public ICollection<User>? Users { get; set; }
+    }
+}
--- backend/admin-service/Models/User.cs
+++ backend/admin-service/Models/User.cs
@@ -1,6 +1,4 @@
-﻿using System.Data;
-
-namespace admin_service.Models
+﻿namespace admin_service.Models
 {
     public class User
     {
