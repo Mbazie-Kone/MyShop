76d591667e8f79328e14f3bb3508f91eda77219f
M'bazie Kone <mbazie89@gmail.com>
2025-03-29 14:35:16 +0100

    Updates

--- frontend/src/app/admin/admin.service.ts
+++ frontend/src/app/admin/admin.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
+  private apiUrl = `${environment.apiUrl}/admin`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  register(data: any) {
+
+    return this.http.post(`${this.apiUrl}/register`, data);
+  }
+
+  login(data: any) {
+
+    return this.http.post(`${this.apiUrl}/login`, data);
+  }
 }
