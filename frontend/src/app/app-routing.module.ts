79c8051fec7080864f28287503085c0f682a110b
M'bazie Kone <mbazie89@gmail.com>
2025-03-30 10:52:47 +0200

    Updates

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: '**'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
