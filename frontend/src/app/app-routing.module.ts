9041857289ac4a6e37593cf95bf282d1511c829a
M'bazie Kone <mbazie89@gmail.com>
2025-03-31 21:09:19 +0200

    Updates

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './admin/pages/login/login.component';
 import { RegisterComponent } from './admin/pages/register/register.component';
 import { AdminLayoutComponent } from './core/components/admin/layout/admin-layout.component';
+import { LayoutComponent } from './core/components/myshop/layout/layout.component';
 
 const routes: Routes = [
   { path: 'admin', redirectTo: 'admin/login', pathMatch: 'full'},
@@ -16,6 +17,13 @@ const routes: Routes = [
       { path: '',  loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }
     ]
   },
+  { 
+    path: '',
+    component: LayoutComponent,
+    children: [
+      
+    ]
+  }
   { path: '**', redirectTo: 'catalog', pathMatch: 'full'}
 ];
 
