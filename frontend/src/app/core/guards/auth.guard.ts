2ce5120aa6eb9610c0e1030e28f3a49a2a26df92
M'bazie Kone <mbazie89@gmail.com>
2025-04-04 19:10:57 +0200

    Updates

--- frontend/src/app/core/guards/auth.guard.ts
+++ frontend/src/app/core/guards/auth.guard.ts
@@ -5,5 +5,15 @@ import { AuthService } from '../services/auth.service';
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
+
+  const token = authService.getToken();
+
+  if(!token) {
+    router.navigate(['/admin/login']);
+
+    return false;
+  }
+
+
   return true;
 };
