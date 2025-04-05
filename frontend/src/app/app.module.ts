7ec2b7ccf68432544d4cc925f9922d595cf741f9
M'bazie Kone <mbazie89@gmail.com>
2025-04-05 10:49:49 +0200

    Updates

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminModule } from './admin/admin.module';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 import { MyshopModule } from './myshop/myshop.module';
 import { ErrorsModule } from './errors/errors.module';
 import { RouterModule } from '@angular/router';
-import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [
