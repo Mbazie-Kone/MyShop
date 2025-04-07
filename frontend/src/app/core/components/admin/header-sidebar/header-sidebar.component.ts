a52e36553c054bd1015137feecff2150535ba0dd
M'bazie Kone <mbazie89@gmail.com>
2025-04-07 23:24:03 +0200

    Updates

--- frontend/src/app/core/components/admin/header-sidebar/header-sidebar.component.ts
+++ frontend/src/app/core/components/admin/header-sidebar/header-sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header-sidebar',
@@ -7,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './header-sidebar.component.css'
 })
 export class HeaderSidebarComponent {
-  
+  isMenuOpen = false;
+
+  constructor(private authService: AuthService, private router: Router) {}
 }
