784056e127bca54abd2efe3685a29ae1ca4555e7
M'bazie Kone <mbazie89@gmail.com>
2025-04-19 19:37:20 +0200

    Updates

--- backend/myshop/catalog-service/Controllers/CatalogController.cs
+++ backend/myshop/catalog-service/Controllers/CatalogController.cs
@@ -1,5 +1,6 @@
 ﻿using catalog_service.Data;
 using catalog_service.DTOs;
+using catalog_service.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace catalog_service.Controllers
@@ -27,7 +28,30 @@ namespace catalog_service.Controllers
             var category = await _context.Categories.FindAsync(dto.CategoryId);
             if (category == null)
                 return NotFound("Category not found.");
-           
+
+            var product = new Product
+            {
+                Name = dto.Name,
+                Description = dto.Description,
+                Price = dto.Price,
+                Stock = dto.Stock,
+                IsAvailable = dto.IsAvailable,
+                CategoryId = dto.CategoryId,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow,
+                Images = dto.ImageUrls != null ? dto.ImageUrls.Select(url => new Image
+                {
+                    Url = url,
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedAt = DateTime.UtcNow,
+                }).ToList() : new List<Image>()
+            };
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProductById), new {  id = product.Id }, product);
+            
         }
     }
 }
