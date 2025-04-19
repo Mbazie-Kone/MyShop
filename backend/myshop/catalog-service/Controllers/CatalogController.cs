6e65681ba628b0530784ba4fc9a5732e51eb1985
M'bazie Kone <mbazie89@gmail.com>
2025-04-19 19:23:36 +0200

    Updates

--- backend/myshop/catalog-service/Controllers/CatalogController.cs
+++ backend/myshop/catalog-service/Controllers/CatalogController.cs
@@ -1,7 +1,14 @@
-﻿namespace catalog_service.Controllers
-{
-    public class CatalogController
-    {
+﻿using Microsoft.AspNetCore.Mvc;
 
+namespace catalog_service.Controllers
+{
+    [Route("api/catalog")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        public CatalogController()
+        {
+
+        }
     }
 }
