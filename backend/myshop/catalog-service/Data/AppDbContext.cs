8bb5d52812cb578fad67323a05c4544ffee019fd
M'bazie Kone <mbazie89@gmail.com>
2025-04-19 11:53:11 +0200

    Updates

--- backend/myshop/catalog-service/Data/AppDbContext.cs
+++ backend/myshop/catalog-service/Data/AppDbContext.cs
@@ -10,5 +10,24 @@ namespace catalog_service.Data
         public DbSet<Category> Categories => Set<Category>();
         public DbSet<Product> Products => Set<Product>();
         public DbSet<Image> Images => Set<Image>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                .HasIndex(c => c.Name)
+                .IsUnique();
+
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany(p => p.Products)
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Image>()
+                .HasOne(i => i.Product)
+                .WithMany(i => i.Images)
+                .HasForeignKey(i => i.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
