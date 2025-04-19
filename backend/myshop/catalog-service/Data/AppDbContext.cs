219e06e177ff6e78113e545161f9d937547a8c1d
M'bazie Kone <mbazie89@gmail.com>
2025-04-19 11:48:05 +0200

    Updates

--- backend/myshop/catalog-service/Data/AppDbContext.cs
+++ backend/myshop/catalog-service/Data/AppDbContext.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using catalog_service.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace catalog_service.Data
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Image> Images => Set<Image>();
     }
 }
