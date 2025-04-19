74cc43eebd7b34c60b1fe344b2857f48fe188088
M'bazie Kone <mbazie89@gmail.com>
2025-04-19 23:13:25 +0200

    Updates

--- backend/myshop/catalog-service/DTOs/InsertCategoryProductImageDto.cs
+++ backend/myshop/catalog-service/DTOs/InsertCategoryProductImageDto.cs
@@ -11,9 +11,11 @@ namespace catalog_service.DTOs
         [StringLength(500, ErrorMessage = "Description can't exceed 500 characters.")]
         public string Description { get; set; } = null!;
 
+        [Required(ErrorMessage = "Price is required")]
         [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
         public decimal Price { get; set; }
 
+        [Required(ErrorMessage = "Stock is required")]
         [Range(0, int.MaxValue, ErrorMessage ="Stock must be a positive number.")]
         public int Stock {  get; set; }
 
