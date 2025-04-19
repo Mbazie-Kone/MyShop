63cc7bc4b655700992195239dbabb9409673ec20
M'bazie Kone <mbazie89@gmail.com>
2025-04-19 14:51:40 +0200

    Updates

---
+++ backend/myshop/catalog-service/DTOs/InsertCategoryProductImageDto.cs
@@ -0,0 +1,15 @@
+﻿namespace catalog_service.DTOs
+{
+    public class InsertCategoryProductImageDto
+    {
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int Stock {  get; set; }
+        public bool IsAvailable { get; set; }
+        public long CategoryId { get; set; }
+
+        // At most 10 images (validated in the controller)
+        public List<string>? ImageUrls { get; set; }
+    }
+}
