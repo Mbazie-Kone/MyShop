01e5c8cd4d50fe7e8f6bf9375f8f7c080874af73
M'bazie Kone <mbazie89@gmail.com>
2025-04-19 13:07:04 +0200

    Updates

---
+++ backend/myshop/catalog-service/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using catalog_service.Data;
+using catalog_service.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace catalog_service.Controllers
+{
+    [Route("api/catalog")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("categories")]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+    }
+}
