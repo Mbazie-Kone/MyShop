6c4956c31c9f1e8420f8b7eefcf13dbb636ad20d
M'bazie Kone <mbazie89@gmail.com>
2025-04-22 01:18:57 +0200

    Updates

--- frontend/src/app/admin/pages/products/products.component.ts
+++ frontend/src/app/admin/pages/products/products.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
@@ -6,6 +8,46 @@ import { Component } from '@angular/core';
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  productForm!: FormGroup;
+  selectedFiles: File[] = [];
+  categories: any[] = [];
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
+  
+  ngOnInit() {
+    this.productForm = this.fb.group({
+      name: ['', Validators.required],
+      description: [''],
+      price: [0, Validators.required],
+      stock: [0, Validators.required],
+      isAvailable: [true],
+      categoryId: [null, Validators.required]
+    });
+
+    /*
+    this.http.get<any[]>('http://localhost:5000/api/catalog/categories')
+      .subscribe(res => this.categories = res);
+    */
+  }
+
+  onFileSelected(event: any) {
+    this.selectedFiles = Array.from(event.target.files);
+  }
+
+  onSubmit() {
+    const formData = new FormData();
+    Object.entries(this.productForm.value).forEach(([key, value]) => {
+      formData.append(key, value as string);
+    });
+    this.selectedFiles.forEach(file => {
+      formData.append('Images', file);
+    })
+
+    /*
+    this.http.post('http://localhost:5000/api/catalog/add-product', formData)
+      .subscribe(() => alert("Product added!"));
+    */
+  }
 
 }
