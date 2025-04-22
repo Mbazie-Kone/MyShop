e2a41e1b354d13db22a38e6eea8705d7b40fa338
M'bazie Kone <mbazie89@gmail.com>
2025-04-22 02:39:45 +0200

    Updates

--- frontend/src/app/core/models/catalog.model.ts
+++ frontend/src/app/core/models/catalog.model.ts
@@ -1,11 +1,24 @@
 export interface Product {
-
+    id?: number;
+    name: string;
+    description?: string;
+    price: number;
+    stock: number;
+    isAvailable: boolean;
+    categoryId: number;
+    createdAt?: Date;
+    updatedAt?: Date;
+    images?: Image[];
 }
 
 export interface Image {
-
+    id?: number;
+    url: string;
+    productId?: number;
+    createdAt?: Date;
+    updatedAt?: Date;
 }
 
 export interface Category {
-    
+
 }
