9a08eaae2f338e8a9e2e68b31a15ba59bab1dcf3
M'bazie Kone <mbazie89@gmail.com>
2025-04-22 03:09:42 +0200

    Updates

--- frontend/src/app/admin/services/catalog.service.ts
+++ frontend/src/app/admin/services/catalog.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Category, Product } from '../../core/models/catalog.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,12 @@ export class CatalogService {
 
   constructor(private http: HttpClient) { }
 
-  createProduct(formData: FormData): Observable<any> {
-    return this.http.post(`${this.apiUrl}/add-product`, formData);
+  getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${this.apiUrl}/categories`);
   }
 
-  getCategories(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/categories`);
+  createProduct(formData: FormData): Observable<Product> {
+    return this.http.post<Product>(`${this.apiUrl}/add-product`, formData);
   }
+
 }
