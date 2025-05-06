35e3575d5d7ad7677239efae29fd7e1bdd5efb74
M'bazie Kone <mbazie89@gmail.com>
2025-05-06 19:48:42 +0200

    Updates

--- initdb/entrypoint.sh
+++ initdb/entrypoint.sh
@@ -6,12 +6,12 @@ sleep 20
 
 echo "Executing init scripts..."
 
-for script in /backend/myshop/admin-service/*.sql; do
+for script in /initdb/admin-service/*.sql; do
 	echo "Running $script"
 	/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -p $SA_PASSWORD -i $script
 done
 
-for script in /backend/myshop/catalog-service/*.sql; do
+for script in /initdb/catalog-service/*.sql; do
 	echo "Running $script"
 	/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -p $SA_PASSWORD -i $script
 done
