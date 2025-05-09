496ac9483b473a57c6136b264675151ed8f2a9cc
M'bazie Kone <mbazie89@gmail.com>
2025-05-09 13:47:36 +0200

    Updates

--- initdb/entrypoint.sh
+++ initdb/entrypoint.sh
@@ -6,12 +6,22 @@ set +H
 echo "Waiting for SQL Server to start..."
 sleep 20
 
+db_exists() {
+	/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -Q "IF DB_ID('$1') IS NOT NULL PRINT 'YES'" -h -1
+}
+
 echo "Executing init scripts..."
 
-for script in /initdb/admin-service/*.sql; do
-	echo "Running $script"
-	/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -d master -i "$script"
-done
+# Run init.sql of admin-service only if the admindb database does not exist.
+if [ "$(db_exists admindb)" != "YES" ]; then
+    echo "Database admindb not found, creating it..."
+	for script in /initdb/admin-service/*.sql; do
+	    echo "Running $script"
+	    /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$SA_PASSWORD" -d master -i "$script"
+	done
+else
+    echo "Database admindb already exists, skipping initialization."
+fi	
 
 for script in /initdb/catalog-service/*.sql; do
 	echo "Running $script"
