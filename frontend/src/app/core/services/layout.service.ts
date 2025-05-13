ebfda0419551df02db503e1b03e009f1f1cacb13
M'bazie Kone <mbazie89@gmail.com>
2025-05-13 02:15:12 +0200

    Updates

--- frontend/src/app/core/services/layout.service.ts
+++ frontend/src/app/core/services/layout.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LayoutService {
+  private _collapsed = new BehaviorSubject<boolean>(false);
+  collapsed$ = this._collapsed.asObservable();
 
-  constructor() { }
+  toggle() {
+    this._collapsed.next(!this._collapsed.value);
+  }
+
+  isCollapsed(): boolean {
+    return this._collapsed.value;
+  }
 }
