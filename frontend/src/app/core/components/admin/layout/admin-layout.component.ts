d76fee65e9761737304052368e98c9ef6ea55d40
M'bazie Kone <mbazie89@gmail.com>
2025-05-24 00:42:41 +0200

    Updates

--- frontend/src/app/core/components/admin/layout/admin-layout.component.ts
+++ frontend/src/app/core/components/admin/layout/admin-layout.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { LayoutService } from '../../../services/layout.service';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-admin-layout',
@@ -8,6 +10,22 @@ import { LayoutService } from '../../../services/layout.service';
   styleUrl: './admin-layout.component.css'
 })
 export class AdminLayoutComponent {
+  pageTitle: string = '';
+
   // COLLAPSED
-  constructor(public layoutService: LayoutService) {}
+  constructor(public layoutService: LayoutService, private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.setPageTitle(this.router.url);
+    });
+  }
+
+  private setPageTitle(url: string): void {
+    if (url.includes('/administration/dashboard')) {
+      this.pageTitle = 'MyShop';
+    } else if (url.includes('/administration/view-products')) {
+      this.pageTitle = 'Products';
+    }
+  }
 }
