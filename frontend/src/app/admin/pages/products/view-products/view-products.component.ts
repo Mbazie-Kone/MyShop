e5484dee0dd33c5a0133f4eab3c08cad6bff5199
M'bazie Kone <mbazie89@gmail.com>
2025-05-24 02:16:03 +0200

    Updates

--- frontend/src/app/admin/pages/products/view-products/view-products.component.ts
+++ frontend/src/app/admin/pages/products/view-products/view-products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PageTitleService } from '../../../../core/services/page-title.service';
 
 @Component({
   selector: 'app-view-products',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './view-products.component.css'
 })
 export class ViewProductsComponent {
+  pageTitle: string = '';
 
+  constructor(private pageTitleService: PageTitleService) {
+    this.pageTitleService.pageTitle$.subscribe(title => {
+      this.pageTitle = title;
+    });
+  }
 }
