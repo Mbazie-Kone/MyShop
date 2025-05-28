5664a741f4e1210aa8daca73176fb1469c0f8f90
M'bazie Kone <mbazie89@gmail.com>
2025-05-28 02:14:57 +0200

    Updates

---
+++ backend/myshop/catalog-service/DTOs/ViewAllProductsDto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace catalog_service.DTOs
+{
+    public class ViewAllProductsDto
+    {
+
+        public string ImageUrl { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
