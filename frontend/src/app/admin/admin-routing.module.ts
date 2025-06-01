208a45edba67691b5e52f0358fecb327de76532a
M'bazie Kone <mbazie89@gmail.com>
2025-06-01 14:33:32 +0200

    Updates

--- frontend/src/app/admin/admin-routing.module.ts
+++ frontend/src/app/admin/admin-routing.module.ts
@@ -4,11 +4,13 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { authGuard } from '../core/guards/auth.guard';
 import { ViewProductsComponent } from './pages/products/view-products/view-products.component';
 import { AddProductComponent } from './pages/products/add-product/add-product.component';
+import { EditProductComponent } from './pages/products/edit-product/edit-product.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent, data: { title: 'Dashboard - MyShop' }, canActivate: [authGuard] },
-  { path: 'view-products', component: ViewProductsComponent, data: { title: 'Products List - MyShop' }},
-  { path: 'add-product', component: AddProductComponent}
+  { path: 'view-products', component: ViewProductsComponent, data: { title: 'Products List - MyShop' }, },
+  { path: 'add-product', component: AddProductComponent, data: { title: 'Add Product - MyShop' }, },
+  { path: 'edit-product/:id', component: EditProductComponent, data: { title: 'Edit Product - MyShop' }, }
 ];
 
 @NgModule({
--- frontend/src/app/admin/pages/products/edit-product/edit-product.component.ts
+++ frontend/src/app/admin/pages/products/edit-product/edit-product.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Category } from '../../../../core/models/catalog.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CatalogService } from '../../../services/catalog.service';
 
 @Component({
   selector: 'app-edit-product',
@@ -6,6 +10,31 @@ import { Component } from '@angular/core';
   templateUrl: './edit-product.component.html',
   styleUrl: './edit-product.component.css'
 })
-export class EditProductComponent {
+export class EditProductComponent implements OnInit {
+  productForm!: FormGroup;
+  categories: Category[] = [];
+  SelectedFiles: File[] = [];
+  productId!: number;
+  existingImages: string[] = [];
+
+  constructor(private route: ActivatedRoute, private catalogService: CatalogService, private fb: FormBuilder, private router: Router) {}
+ 
+  ngOnInit(): void {
+   this.productId = +this.route.snapshot.paramMap.get('id')!;
+   this.loadCategories();
+   this.loadProduct();
+   this.initForm();
+  }
+
+  initForm() {
+    this.productForm = this.fb.group({
+      name: ['', Validators.required ],
+      description: [''],
+      price: [0, [Validators.required, Validators.min(0)]],
+      stock: [0, [Validators.required, Validators.min(0)]],
+      categoryId: [null, Validators.required],
+      Images: [null]
+    });
+  }
 
 }
