54041ef5ec3b6d6ee09e1bdbcddb014692a773ba
M'bazie Kone <mbazie89@gmail.com>
2025-06-04 01:55:30 +0200

    Updates

---
+++ backend/myshop/catalog-service/Services/IImageService.cs
@@ -0,0 +1,10 @@
+﻿using catalog_service.Models;
+
+namespace catalog_service.Services
+{
+    public interface IImageService
+    {
+        Task<List<Image>> SaveImagesAsync(IFormFileCollection files, int productId);
+        void DeleteImage(List<Image> images);
+    }
+}
---
+++ backend/myshop/catalog-service/Services/Impl/ImageServiceImpl.cs
@@ -0,0 +1,17 @@
+﻿using catalog_service.Models;
+
+namespace catalog_service.Services.Impl
+{
+    public class ImageServiceImpl : IImageService
+    {
+        public void DeleteImage(List<Image> images)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Image>> SaveImagesAsync(IFormFileCollection files, int productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
