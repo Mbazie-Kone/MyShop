1128f85227771d52bbcb44f14455776ea13011e9
M'bazie Kone <mbazie89@gmail.com>
2025-06-09 20:41:56 +0200

    Updates

--- frontend/src/app/core/components/admin/product-form/product-form.component.ts
+++ frontend/src/app/core/components/admin/product-form/product-form.component.ts
@@ -31,6 +31,27 @@ export class ProductFormComponent implements OnInit {
       categoryId: [null, Validators.required],
       images: [null]
     });
+
+    this.loadCategories();
+
+    this.route.paramMap.subscribe(params => {
+      const idParam = params.get('id');
+      if (idParam) {
+        this.isEditMode = true;
+        this.productId = +idParam;
+        this.loadProduct(this.productId);
+      }
+    });
+
+    this.productForm.get('stock')?.valueChanges.subscribe(stock => {
+      const priceControl = this.productForm.get('price');
+      if (stock > 0) {
+        priceControl?.enable();
+      } else {
+        priceControl?.disable();
+        priceControl?.setValue(0);
+      }
+    });
   }
 
 }
