7cdcd6871ed831ced27196e5ac8af425c85e4192
M'bazie Kone <mbazie89@gmail.com>
2025-07-01 19:27:29 +0200

    Updates

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from './spinner/spinner.component';
 import { CardComponent } from './card/card.component';
+import { CardHeaderComponent } from './card/card-header/card-header.component';
 
 @NgModule({
-  declarations: [SpinnerComponent, CardComponent],
+  declarations: [SpinnerComponent, CardComponent, CardHeaderComponent],
   imports: [
     CommonModule
   ],
